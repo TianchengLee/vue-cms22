2fc45a9ba8308a4312e50ee2ec5777d1dfe3b8dc
TianchengLee <ltc6634284@gmail.com>
2018-12-31 14:22:15 +0800

    项目清理完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// @符号表示 src 目录
-import HelloWorld from '@/components/HelloWorld'
+// @ 符号表示 src 目录
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
   ]
 })
